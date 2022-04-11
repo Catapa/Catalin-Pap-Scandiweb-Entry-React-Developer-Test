137e31de493f3213279313fd3044541976afdf5e
Catapa <catalin24pap@gmail.com>
2022-04-11 17:25:51 +0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import {useQuery} from "@apollo/client";
-import {CATEGORY_NAMES} from "./Queries/queries";
+import styles from './styles.module.css';
+import Header from "./Components/Header/Header";
 
 function App() {
-  const {loading, error, data} = useQuery(CATEGORY_NAMES);
   return (
-   <p>
-     {console.log(data.categories)}
-     placeholder
-   </p>
+    <Header/>
   );
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React, {PureComponent} from 'react';
+import styles from './Header.module.css';
+
+export class Header extends PureComponent {
+    render () {
+        return (
+            <header className={styles.header}>
+                <nav className={styles.navigation}>
+                    <ul className={styles.category_list}>
+                        <li className={styles.category_list_item}><a>women</a></li>
+                        <li className={styles.category_list_item}><a>men</a></li>
+                        <li className={styles.category_list_item}><a>kids</a></li>
+                    </ul>
+                </nav>
+
+                <img src={'assets/logo.svg'} alt={'logo'} className={styles.logo}/>
+
+                <nav className={styles.navigation}>
+                    <ul className={styles.action_list}>
+                        <li className={styles.action_list_item}>
+                            <img src={'assets/dollar_sign.svg'} alt={'currency is dollar'} className={styles.action_list_item__currency}/>
+                            <img src={'assets/arrow_down.svg'} alt={'select currency'} className={styles.action_list_item__arrow}/>
+                        </li>
+                        <li className={styles.action_list_item}>
+                            <img src={'assets/empty_cart.svg'} alt={'cart'} className={styles.action_list_item_cart}/>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Header;
