d7bcc186165422c28733e3c620c1a9e70daedef1
Catapa <catalin24pap@gmail.com>
2022-04-16 13:08:38 +0300

    add CartPage, ProductDescriptionPage and ProductListingPage as empty class components

---
+++ src/Pages/CartPage/CartPage.jsx
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+export class CartPage extends PureComponent {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CartPage;
---
+++ src/Pages/ProductDescriptionPage/ProductDescriptionPage.jsx
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+export class ProductDescriptionPage extends PureComponent {
+    render () {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ProductDescriptionPage;
