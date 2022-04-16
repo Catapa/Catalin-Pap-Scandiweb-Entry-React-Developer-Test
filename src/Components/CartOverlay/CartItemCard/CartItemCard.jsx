f142466619f5e38dccdf81b3024689cdf5eee348
Catapa <catalin24pap@gmail.com>
2022-04-16 13:02:35 +0300

    improve styling on CartOverlay and CartItemCard components

--- src/Components/CartOverlay/CartItemCard/CartItemCard.jsx
+++ src/Components/CartOverlay/CartItemCard/CartItemCard.jsx
@@ -5,7 +5,24 @@ export class CartItemCard extends PureComponent {
     render () {
         return (
             <div className={styles.item_card}>
-                    Item in cart
+                <div className={styles.product_info}>
+                    <div>
+                        <p className={styles.product_info__name}>Apollo Running Short</p>
+                        <p className={styles.product_info__price}>$50.00</p>
+                    </div>
+                    <div className={styles.product_info__sizes}>
+                        <button className={styles.button}>S</button>
+                        <button className={`${styles.button} ${styles.button_disabled}`}>M</button>
+                    </div>
+                </div>
+
+                <div className={styles.quantity_selector}>
+                    <button className={styles.button}>-</button>
+                    <span className={styles.quantity_label}>1</span>
+                    <button className={styles.button}>+</button>
+                </div>
+
+                <img src={'./assets/demo_cart.svg'} alt={'product'}  className={styles.image}/>
             </div>
         )
     }
