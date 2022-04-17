0dd1933e76944223499f30a77b0e592ff23debd7
Catapa <catalin24pap@gmail.com>
2022-04-17 16:08:10 +0300

    add routing to product detail page - placeholder

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,18 +1,29 @@
 import React, {PureComponent} from 'react';
 import styles from './ProductCard.module.css';
+import {Link} from "react-router-dom";
 
 export class ProductCard extends PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    gotoProductPage() {
+
+    }
+
     render () {
         return (
-            <div className={styles.product_card}>
-                <img src={'assets/demo_product.svg'} alt={'product image'} className={styles.product_image}/>
-                <button className={styles.product_buy_button}>
-                    <img src={'assets/empty_cart_white.svg'} alt={'Add to cart'}/>
-                </button>
-                <span className={styles.product_name}>Apollo Running Short</span>
-                <span className={styles.product_price}>$50.00</span>
-            </div>
-        )
+            <Link to={'/product'}>
+                <div className={styles.product_card}>
+                    <img src={'assets/demo_product.svg'} alt={'product image'} className={styles.product_image}/>
+                    <button className={styles.product_buy_button}>
+                        <img src={'assets/empty_cart_white.svg'} alt={'Add to cart'}/>
+                    </button>
+                    <span className={styles.product_name}>Apollo Running Short</span>
+                    <span className={styles.product_price}>$50.00</span>
+                </div>
+            </Link>
+        );
     }
 }
 
