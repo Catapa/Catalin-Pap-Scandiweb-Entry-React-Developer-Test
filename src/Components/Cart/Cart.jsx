388298e41e5f9d5cd62a1f5247430596c882f59e
Catapa <catalin24pap@gmail.com>
2022-06-16 20:03:19 +0300

    close CartOverlay when clicked outside

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -12,6 +12,8 @@ class Cart extends PureComponent {
         this.state = {
             showCartOverlay: false
         }
+        this.containerRef = React.createRef();
+        this.closeCartOverlay = this.closeCartOverlay.bind(this);
     }
     toggleCartOverlay = () => {
         if (this.state.showCartOverlay === true) {
@@ -25,15 +27,21 @@ class Cart extends PureComponent {
             })
         }
     }
-    closeCartOverlayOnBlur = (event) => {
-        if (!event.currentTarget.contains(event.relatedTarget)) {
-            this.setState({
-                showCartOverlay: false
-            })
+    closeCartOverlay = (event) => {
+        try {
+            if (this.containerRef && !this.containerRef.current.contains(event.target)) {
+                this.setState({
+                    showCartOverlay: false
+                })
+            }
+        }
+        catch (error) {
+            console.log(error);
         }
     }
     componentDidMount() {
         try {
+            document.addEventListener('mousedown', this.closeCartOverlay, true);
             const productsInCart = JSON.parse(window.sessionStorage.getItem("productsInCart"));
             this.context.setData({productsInCart: productsInCart});
         }
@@ -41,6 +49,15 @@ class Cart extends PureComponent {
             console.log(error);
         }
     }
+    componentWillUnmount() {
+        try {
+            document.removeEventListener('mousedown', this.closeCartOverlay, true);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
     render() {
         const productsInCart = JSON.parse(window.sessionStorage.getItem("productsInCart"));
         const cartItemsCounter = productsInCart.reduce(
@@ -50,14 +67,17 @@ class Cart extends PureComponent {
             , 0
         );
         return (
-            <span tabIndex={4} onBlur={(e) => this.closeCartOverlayOnBlur(e)}>
+            <span tabIndex={4} ref={this.containerRef}>
+                {/* Backdrop */}
                 <Backdrop visible={this.state.showCartOverlay} onClick={this.toggleCartOverlay}/>
+                {/* Cart Icon */}
                 <span onClick={this.toggleCartOverlay} className={styles.container}>
                     <img src={empty_cart_black} alt={'cart'}
                          className={styles.cart_image}/>
                          <span className={styles.items_counter}
                                style={{display: (cartItemsCounter) ? "flex" : "none"}}>{cartItemsCounter}</span>
                 </span>
+                {/* Cart Overlay */}
                 <CartOverlay visible={this.state.showCartOverlay} toggleCartOverlay={this.toggleCartOverlay}/>
             </span>
         );
